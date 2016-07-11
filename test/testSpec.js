f47c0d0ba29a9942a3e5cde83f9ee8e1030450a1
Gunter Spranz <gunter@gspranz.de>
2016-07-11 13:43:16 -0700

    Add dummy test Add npm script targets Add empty index.js Add eslint config

---
+++ test/testSpec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+//const restify = require('restify-errors');
+//const middleware = require('../');
+//const expect = require('chai').expect;
+
+describe('test', function() {
+  it('passes', function() {
+  });
+});
