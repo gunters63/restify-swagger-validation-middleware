8d3b3935dae03215348585f7f077667b7e2d5ada
Gunter Spranz <gunter@gspranz.de>
2016-07-11 14:52:40 -0700

    Added dummy middleware and test

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 'use strict';
+
+const Ajv = require('ajv');
+const restify = require('restify-errors');
+
+const defaultErrorTransformer = (input, errors) => {
+  let result = new restify.BadRequestError('Validation error');
+  result.errors = errors;
+  return result;
+};
+
+const defaultErrorResponder = (transformedErr, req, res, next) => {
+  next(transformedErr);
+};
+
+module.exports = function (options, swagger) {
+  options = options || {};
+  swagger = swagger || {};
+  let ajvOptions = options.ajv || {
+      v5: true,
+      allErrors: true,
+      useDefaults: true,
+      coerceTypes: true
+    };
+
+  let errorTransformer = options.errorTransformer || defaultErrorTransformer;
+  let errorResponder = options.errorResponder || defaultErrorResponder;
+
+  let ajv = new Ajv(ajvOptions);
+
+  return function restifyMiddleware(req, res, next) {
+    let dataToValidate = {};
+    let validate = ajv.compile({});
+    let valid = validate(dataToValidate);
+
+    if (!valid) {
+      errorResponder(errorTransformer(dataToValidate, validate.errors), req, res, next);
+      return;
+    }
+
+    if (next) {
+      next();
+    }
+  };
+};
--- test/testSpec.js
+++ test/testSpec.js
@@ -1,10 +1,21 @@
 'use strict';
 
-//const restify = require('restify-errors');
-//const middleware = require('../');
-//const expect = require('chai').expect;
+const middleware = require('../');
+const expect = require('chai').expect;
+const httpMocks = require('node-mocks-http');
 
-describe('test', function() {
-  it('passes', function() {
+describe('test', function () {
+
+  it('passes', function () {
+    let handler = middleware();
+    let request = httpMocks.createRequest({
+      method: 'GET',
+      url: '/user/42',
+      params: {
+        id: 42
+      }
+    });
+    let response = httpMocks.createResponse();
+    handler(request, response);
   });
 });
